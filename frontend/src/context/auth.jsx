4b71425070da805790c145e93c8a5986aef4ed56
poojawarungase <poojawarungase30@gmail.com>
2026-05-17 17:46:17 +0530

    Update auth.jsx

--- frontend/src/context/auth.jsx
+++ frontend/src/context/auth.jsx
@@ -6,7 +6,7 @@ export const authContext = createContext();
 
 const Auth = ({ children }) => {
 
-  let serverUrl = "http://localhost:8000";
+  let serverUrl = "https://havnlybackend.vercel.app/";
 
   let value = {
     serverUrl
